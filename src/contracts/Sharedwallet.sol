06a5f42cdb143cde9b0c1335fc0f839fd3b7a98b
Miguel Gomez <mgomezb89@gmail.com>
2021-03-24 16:29:31 +0100

    add events to wallet and allowance contracts

--- src/contracts/Sharedwallet.sol
+++ src/contracts/Sharedwallet.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Allowance is Ownable {
 
+    event AllowanceChanged(address indexed _forWho, address indexed _byWhom, uint _oldAmount, uint _newAmount);
+
     // view who the owner of the contract is
     function isOwner() internal view returns(bool) {
         return owner() == msg.sender;
@@ -15,11 +17,13 @@ contract Allowance is Ownable {
     
     //add allowance to recipient
     function addAllowance(address _who, uint _amount) public onlyOwner {
-        allowance[_who] = _amount;
+        AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] + _amount);
+        allowance[_who] += _amount;
     }
     
     // reduce allowance from recipient on withdrawal, function is private
     function reduceAllowance(address _who, uint _amount) internal ownerOrAllowed(_amount) {
+        AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
         allowance[_who] -= _amount;
     }
     
@@ -28,10 +32,13 @@ contract Allowance is Ownable {
         require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed");
         _;
     }
-    
+
 }
 
 contract SharedWallet is Allowance {
+
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
     
     // withdrawal function with instructions 
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
@@ -40,11 +47,14 @@ contract SharedWallet is Allowance {
         if (!isOwner()) {
             reduceAllowance(msg.sender, _amount);
         }
+
+        MoneySent(msg.sender, _amount);
         
         _to.transfer(_amount);
     }
 
     receive() external payable {
+        MoneyReceived(msg.sender, msg.value);
         
     }
     
