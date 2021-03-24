613914a95c2108b7fb9810d5c2486e0cdac70914
Miguel Gomez <mgomezb89@gmail.com>
2021-03-24 15:56:01 +0100

    add allowance map and improved withdrawal functionality

--- src/contracts/Sharedwallet.sol
+++ src/contracts/Sharedwallet.sol
@@ -9,10 +9,23 @@ contract SharedWallet is Ownable {
     function isOwner() internal view returns(bool) {
         return owner() == msg.sender;
     }
-
+    
+    mapping(address => uint) public allowance;
+    
+    //add allowance to recipient
+    function addAllowance(address _who, uint _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
+    
+    // check if owner or if allowance recipient has funds
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed");
+        _;
+    }
     
     // withdrawal function with instructions 
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(_amount <= address(this).balance, "Contract doesn't own enough funds");
         _to.transfer(_amount);
     }
 
@@ -20,4 +33,4 @@ contract SharedWallet is Ownable {
         
     }
     
-}
+}   
